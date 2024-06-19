f027b126eeb25da470fd7212394ffc2308958dd8
Alex Barnes <164943873+AlexOBarnes@users.noreply.github.com>
2024-06-19 22:50:54 +0100

    Add files via upload
    Uploaded outline of project, dictionary and hangman art

---
+++ main.py
@@ -0,0 +1,30 @@
+from hangman_art import drawing
+
+
+def main():
+    pass
+
+
+def welcome_message():
+    pass
+
+
+def word_select():
+    pass
+
+
+def user_guess():
+    pass
+
+
+def guess_check():
+    pass
+
+
+def display():
+    pass
+
+
+if __name__ == "__main__":
+    welcome_message()
+    main()
