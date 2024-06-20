e3c570e6c67833638c87a53bae5962f0f45d4291
Alex Barnes <a.o.barnes222@gmail.com>
2024-06-20 13:14:52 +0100

    Added win_,lose_check and user_choice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from random import choice
 from hangman_art import drawing
+from replit import clear
+import time
 
 
 def main():
@@ -14,7 +16,6 @@ def main():
             guess_count = guess_check(chosen_word, guess, guess_count)
             is_endgame = display(chosen_word, guessed_letters, guess_count)
         user_choice()
-    pass
 
 
 def welcome_message():
@@ -44,14 +45,14 @@ def user_guess():
 
 
 def guess_check(guess, count):
-    if guess in chosen_word and guess not in guessed_letters:
+    if guess in word and guess not in guessed_letters:
         print(f"\nNice work! {guess.upper()} is in the word!\n")
         guessed_letters.append(guess)
         return count+1
-    elif guess not in chosen_word and guess not in wrong_letters:
+    elif guess not in word and guess not in wrong_letters:
         print(f"\nOh no! There is no {guess.upper()} in this word!\n")
         wrong_letters.append(guess)
-        hangman(wrong_letters)
+        drawing(len(wrong_letters), wrong_letters)
         return count+1
     else:
         print("\nYou already guessed that letter! Try again!")
@@ -59,32 +60,62 @@ def guess_check(guess, count):
 
 
 def win_check(dash, sentence, count):
-    pass
-
-
-def hangman(wrong_letters):
-    pass
+    if dash not in sentence:
+        print("Well done! You have won!")
+        print(f"It took you only {count} guesses!")
+        return True
+    else:
+        return False
 
 
 def lose_check():
-    pass
+    if len(wrong_letters) == 8:
+        print("\nOh no! You have lost!")
+        print(f"The word I was thinking of was {chosen_word}")
+        return True
+    else:
+        return False
 
 
 def display(chosen_word, guessed_letters, guess_count):
     dash = "___ "
     sentence = []
+    is_solved = False
+    is_lost = False
     for letter in chosen_word:
         if letter in guessed_letters:
             sentence.append(letter)
             sentence.append(" ")
         else:
             sentence.append(dash)
-    print("\n")
     print("".join(sentence))
-    return win_check(dash, sentence, guess_count)
+    if count > 0:
+        is_solved = win_check(dash, sentence, guess_count)
+        is_lost = lose_check()
+    if is_solved or is_lost:
+        return True
+    else:
+        return False
 
 
 def user_choice():
+    while True:
+        choice = input("\nWould you like to play again? (y/n): ")
+        if choice.lower() == "y":
+            tidy()
+            print("\nYay! Let's play again!")
+            guessed_letters.clear()
+            wrong_letters.clear()
+            print("Alright, I'm thinking of a new word... Okay I've got one!\n")
+            break
+        elif choice.lower() == "n":
+            print("\nThanks for playing! See you next time!")
+            exit()
+        else:
+            print("That is not a valid input. Try Again!")
+
+
+def tidy():
     pass
 
 
