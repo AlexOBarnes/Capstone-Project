a95f6691d27ccf2475bf1f48bb810cf885ce728a
Alex Barnes <a.o.barnes222@gmail.com>
2024-06-20 11:34:46 +0100

    Added additional print statements

--- hangman_art.py
+++ hangman_art.py
@@ -1,4 +1,4 @@
-def drawing(num):
+def drawing(num, wrong_letters):
     images = [r"""
     +---+
         |
@@ -57,3 +57,9 @@ def drawing(num):
         |
   ========="""]
     print(f"The hallows are as follows:\n{images[num-1]}")
+    if num == 1:
+        print(f"\nSo far you have made {num} wrong guess")
+    elif 1 < num < 8:
+        print(f"\nSo far you have made {num} wrong guesses")
+    print(f"The letters you have guessed wrong so far are as follows: {
+          (','.join(wrong_letters)).upper()}\n")
